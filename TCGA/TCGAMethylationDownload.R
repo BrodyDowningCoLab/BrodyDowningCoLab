ca156b6d41f848af516ae8388298ae7e3f029eec
Chris Toh <cenlitoh@gmail.com>
2020-11-17 08:17:14 -0800

    Working copy of chr21 download

--- TCGA/TCGAMethylationDownload.R
+++ TCGA/TCGAMethylationDownload.R
@@ -17,7 +17,7 @@ if (!requireNamespace("openssl", quietly = TRUE))
 # When this program makes the call to Bigquery to retrieve the TCGA data, it will prompt you to 
 # login to your google account.
 
-project<- "test-project-cgc"
+project<- "symbolic-rope-182022"
 
 # Google charges for Bigquery access, but gives you 1TB of queries free per month.
 # The query here will consume about 250 MB, or 1/4000 th of your monthly quota.
@@ -31,25 +31,19 @@ library("ggplot2")
 library("tidyr")
 library("openssl")
 
-BigQuerySQL_start_end <- paste("#standardSQL",
-                         "SELECT",
-                         " * ",
-                         "FROM",
-                         "`isb-cgc.TCGA_hg38_data_v0.DNA_Methylation` cn",
-                         "JOIN",
-                         "`isb-cgc.TCGA_bioclin_v0.Biospecimen` bs",
-                         "ON",
-                         "cn.sample_barcode = bs.sample_barcode",
-                         "WHERE",
-                         "bs.sample_type ='10'",
-                         "GROUP BY",
-                         "chromosome,",
-                         "start_pos,",
-                         "end_pos",
-                         "ORDER BY",
-                         "total DESC",                         
+BigQuerySQL_start_end <- paste(
+"SELECT methyl.sample_barcode, beta_value",
+"FROM",
+  "`isb-cgc.TCGA_hg38_data_v0.DNA_Methylation_chr21` methyl",
+"JOIN",
+  "`isb-cgc.TCGA_bioclin_v0.Biospecimen` bs",
+"ON",
+  "methyl.sample_barcode = bs.sample_barcode",
+"WHERE",
+  "bs.project_short_name ='TCGA-COAD'"                         
 )
 
 
 #Get the data
-TCGA_DNA_Methylation <-query_exec(BigQuerySQL_start_end,project,max_pages=Inf, use_legacy_sql = FALSE)
+TCGA_DNA_Methylation <- bq_project_query(project, BigQuerySQL_start_end)
+data <- bq_table_download(TCGA_DNA_Methylation)
