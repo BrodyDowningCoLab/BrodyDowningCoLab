dd97e7a2b05f6f1ab613caa14fc197466d71e71b
Chris Toh <cenlitoh@gmail.com>
2021-04-20 08:47:25 -0700

    Add remaining plots

--- cnvMetAnalysis/generatePaperFigures.R
+++ cnvMetAnalysis/generatePaperFigures.R
@@ -41,7 +41,7 @@ result <- result %>%
 gp_cleanFull <- ggplot(subset(result, Model_Type %in% c("clean", "full")),
              aes(x = CNV, y = AUC,  fill=Model_Type)) +
   geom_boxplot(position = position_dodge(1)) +
-  geom_dotplot(binaxis = 'y', stackdir = 'center', position = position_dodge(1)) +
+  geom_dotplot(binaxis = 'y', stackdir = 'center', position = position_dodge(1), dotsize = 0.5) +
   ggtitle("Comparison of Clean Versus Full CNV COAD Predictions") + 
   xlab("CNV Location") + ylab("AUC of Predictions") +
   theme(plot.title = element_text(size = 18, face = "bold"), 
@@ -54,7 +54,7 @@ gp_clean1up1down <- ggplot(subset(result, Model_Type %in% c("clean", "1 CNV upst
                        aes(x = CNV, y = AUC,  fill = Model_Type)) + 
   geom_boxplot() +
   geom_dotplot(binaxis = 'y', stackdir = 'center', position = position_dodge(0.75), dotsize = 0.5) +
-  ggtitle("Comparison of Clean Versus Upstream and Downstream Methylation Beta Values COAD Predictions") + 
+  ggtitle("Comparison of Clean Versus \nUpstream and Downstream Methylation \nBeta Values COAD Predictions") + 
   xlab("CNV Location") + ylab("AUC of Predictions") +
   theme(plot.title = element_text(size = 18, face = "bold"), 
         legend.position = "right",
@@ -63,40 +63,37 @@ gp_clean1up1down <- ggplot(subset(result, Model_Type %in% c("clean", "1 CNV upst
 gp_clean1up1down
 
 gp_clean1up2up <- ggplot(subset(result, Model_Type %in% c("1 CNV upstream", "2 CNVs upstream")),
-                           aes(x = CNV, y = AUC,  colour = interaction(Model_Type, CNV), group = CNV)) + facet_wrap( ~ Model_Type) +
-  geom_boxplot( fill="white", notch=FALSE) +
-  geom_jitter( size=1, color="black", width=0.3) +
-  #scale_y_discrete(breaks = c(0.50, 0.51,0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.60, 0.61, 0.62, 0.63, 0.64)) +
-  ggtitle("Comparison of 1 CNV Upstream Versus 2 CNVs Upstream Methylation Beta Values COAD Predictions") + 
+                           aes(x = CNV, y = AUC,  fill = Model_Type)) +
+  geom_boxplot() +
+  geom_dotplot(binaxis = 'y', stackdir = 'center', position = position_dodge(0.75), dotsize = 0.5) +
+  ggtitle("Comparison of 1 CNV Upstream Versus 2 CNVs \nUpstream Methylation Beta Values COAD Predictions") + 
   xlab("CNV Location") + ylab("AUC of Predictions") +
   theme(plot.title = element_text(size = 18, face = "bold"), 
-        legend.position = "none",
+        legend.position = "right",
         axis.text.x = element_text(angle = 90))
 
 gp_clean1up2up
 
 gp_clean1down2down <- ggplot(subset(result, Model_Type %in% c("1 CNV downstream", "2 CNVs downstream")),
-                         aes(x = CNV, y = AUC,  colour = interaction(Model_Type, CNV), group = CNV)) + facet_wrap( ~ Model_Type) +
-  geom_boxplot( fill="white", notch=FALSE) +
-  geom_jitter( size=1, color="black", width=0.3) +
-  #scale_y_discrete(breaks = c(0.50, 0.51,0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.60, 0.61, 0.62, 0.63, 0.64)) +
-  ggtitle("Comparison of 1 CNV Downstream Versus 2 CNVs Downstream Methylation Beta Values COAD Predictions") + 
+                         aes(x = CNV, y = AUC,  fill = Model_Type)) +
+  geom_boxplot() +
+  geom_dotplot(binaxis = 'y', stackdir = 'center', position = position_dodge(0.75), dotsize = 0.5) +
+  ggtitle("Comparison of 1 CNV Downstream Versus 2 CNVs \nDownstream Methylation Beta Values COAD Predictions") + 
   xlab("CNV Location") + ylab("AUC of Predictions") +
   theme(plot.title = element_text(size = 18, face = "bold"), 
-        legend.position = "none",
+        legend.position = "right",
         axis.text.x = element_text(angle = 90))
 
 gp_clean1down2down
 
 gp_clean1up1down2up2down <- ggplot(subset(result, Model_Type %in% c("1 CNV downstream", "2 CNVs downstream", "1 CNV upstream", "2 CNVs upstream")),
-                             aes(x = CNV, y = AUC,  colour = interaction(Model_Type, CNV), group = CNV)) + facet_wrap( ~ Model_Type) +
-  geom_boxplot( fill="white", notch=FALSE) +
-  geom_jitter( size=1, color="black", width=0.3) +
-  #scale_y_discrete(breaks = c(0.50, 0.51,0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.60, 0.61, 0.62, 0.63, 0.64)) +
-  ggtitle("Comparison of 1 CNV & 2 CNVs Upstream Versus 1 CNV & 2 CNVs Downstream Methylation Beta Values COAD Predictions") + 
+                             aes(x = CNV, y = AUC,  fill = Model_Type)) + 
+  geom_boxplot() +
+  geom_dotplot(binaxis = 'y', stackdir = 'center', position = position_dodge(0.75), dotsize = 0.5) +
+  ggtitle("Comparison of 1 CNV & 2 CNVs Upstream Versus \n1 CNV & 2 CNVs Downstream Methylation Beta Values COAD Predictions") + 
   xlab("CNV Location") + ylab("AUC of Predictions") +
   theme(plot.title = element_text(size = 18, face = "bold"), 
-        legend.position = "none",
+        legend.position = "right",
         axis.text.x = element_text(angle = 90))
 
 gp_clean1up1down2up2down
